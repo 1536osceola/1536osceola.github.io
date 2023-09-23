1725cbbb3a5fb885f9ceb3ee48db023fcd81eead
Ko <132860793+khruc-sail@users.noreply.github.com>
2023-09-23 16:53:24 +0000

    09.23.23

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/1536Osceola')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/request')
 def request():
     return render_template('request.html')
+
+@app.route('/shoppinglist')
+def shop():
+    return render_template('shop.html')
+
+if __name__ == "__app__":
+    app.run(debug=True)
